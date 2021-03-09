864793b11d979510c31012f76dbb0d6bcf745101
SergeyKagal <3461995@gmail.com>
2021-03-09 19:22:02 +0300

    feat: add min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array) return 0;
+  if(array.length==0 ||array==undefined)  return 0;
+var min=100000000;
+for(var i=0; i<array.length;i++)
+{
+   if(array[i]<min) min=array[i];
+}
+return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array) return 0;
+  if(array.length==0 ||array==undefined)  return 0;
+  var max=-100000000;
+for(var i=0; i<array.length;i++)
+{
+   if(array[i]>max) max=array[i];
+}
+return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array) return 0;
+  if(array.length==0 ||array==undefined) return 0;
+
+  var sum=0;
+  for(var i=0; i<array.length;i++)
+  sum+=array[i];
+
+  return sum/array.length;
 }
